20bdc61c497569a9894e1b7901fdb33404b25a50
apcaoile <apcaoile@gmail.com>
2023-04-25 12:08:30 +0000

    Fixes load_job_from_db.

--- database.py
+++ database.py
@@ -25,3 +25,19 @@ def load_jobs_from_db():
     #jobs.append(dict(row))
     jobs.append(row._mapping)
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"select * from jobs where id = :val"),
+      {"val": id}
+    )
+    #result = conn.execute(text(f"select * from jobs where id = {id}"))
+  jobs = []
+  for row in result.all():
+    jobs.append(row._mapping)
+  if len(jobs) == 0:
+    return None
+  else:
+    return dict(jobs[0])
+    
